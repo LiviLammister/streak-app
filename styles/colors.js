3e423d2da204ff69bd25223e6e0643abcb0f797b
livilammister <livilammister@gmail.com>
2018-06-25 17:47:43 -0500

    added iOS color palette

---
+++ styles/colors.js
@@ -0,0 +1,10 @@
+export default (palette = {
+    blue: '#007AFF',
+    green: '#4CD964',
+    grey: '#8E8E93',
+    lightBlue: '#5AC8FA',
+    orange: '#FF9500',
+    pink: '#FF2D55',
+    red: '#FF3B30',
+    yellow: '#FFCC00'
+});
