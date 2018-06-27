fd61622878f8949a9b1d7181be33808b94b9a392
livilammister <livilammister@gmail.com>
2018-06-27 10:58:48 -0500

    changed jsx files to js and fixed a linter error

---
+++ components/dashboard/dashboard.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  }
+})
+
+class Dashboard extends Component {
+  constructor (props) {
+    super(props)
+    this.displayName = 'Dashboard'
+    this.state = {}
+  }
+  render () {
+    return (
+      <View>
+        <Text style={styles.text}>
+          Dashboard
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default Dashboard
